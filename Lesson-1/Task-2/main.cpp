ef5af29a2075103e2626db95f9586feccb5d4cf4
Darina Kolesnik <79766852+KaniraD@users.noreply.github.com>
2021-07-12 18:23:00 +0300

    Add Task-2

---
+++ Lesson-1/Task-2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float first_number, second_number; 
+    float sum; //сумма первого и второго числа
+    cout << "Enter the first number: ";
+    cin >> first_number;
+    cout << "Enter the second number: ";
+    cin >> second_number;
+    cout << "Sum: " << first_number + second_number <<endl;
+    system("pause");
+    return 0;
+}
