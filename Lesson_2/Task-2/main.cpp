4c90a1224265039d1a76f3af004da3e0b2e34790
Darina Kolesnik <79766852+KaniraD@users.noreply.github.com>
2021-07-12 22:44:40 +0300

    Added Task-2

---
+++ Lesson_2/Task-2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number=0;
+    cout << "Enter number: ";
+    cin >> number;
+    cout << "Numbers: ";
+    for(int i = 0; i < number; i++){
+        cout << i << ", ";
+    }
+    cout << number << endl;
+    system("pause");
+    return 0;
+}
